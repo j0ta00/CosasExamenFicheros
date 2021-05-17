839c70f6374f2cfe4218a055aeb1301c2facb1af
Juan José Muñoz Arenas <rypperx@gmail.com>
2021-05-17 09:27:53 +0200

    Update Menu.java

--- CosasExamen/src/menu/Menu.java
+++ CosasExamen/src/menu/Menu.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class Menu{
 	static Scanner teclado=new Scanner(System.in);
-	
+	static final String ERROR1="Se ha producido un fallo en la lectura del fichero";
+	static final String ERROR2="Se ha producido un fallo en la escritura del fichero";
+	static final String ERROR3="Se ha producido un fallo en la gestiÃ³n del fichero";
 	
 	/**
 	 * Cabecera:public static int MenuPrincipal(int x,int y)
-	 * Proposito:Imprime el menú principal y válida la respuesta del usuario
+	 * Proposito:Imprime el menÃº principal y vÃ¡lida la respuesta del usuario
 	 * @param:int x,int y 
 	 * @return:int respuesta
 	 * 
-	 * Precondición:Ninguna
-	 * Postcondición:Se trata de una función que imprime un menú y valida la respuesta introducida por teclado
+	 * PrecondiciÃ³n:Ninguna
+	 * PostcondiciÃ³n:Se trata de una funciÃ³n que imprime un menÃº y valida la respuesta introducida por teclado
 	 * */
 	public static int MenuPrincipal(int x,int y){
 		int respuesta=0;
@@ -24,5 +26,14 @@ public class Menu{
 		}
 		return respuesta;
 	}
+
+}
+
+public static void imprimirError(int numeroError){
+switch(numeroError){
+	case 1->System.out.println(ERROR1);
+	case 2->System.out.println(ERROR2);
+	case 3->System.out.println(ERROR3);
+}
 
 }
